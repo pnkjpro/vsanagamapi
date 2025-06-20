1cd75586162d57b43479f3a54b4cb426e289c16c
pankajfapl <pankaj.pandey@floweraura.com>
2025-06-21 02:20:11 +0530

    minor changes

--- database/migrations/2025_06_19_094534_create_questions_table.php
+++ database/migrations/2025_06_19_094534_create_questions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('topic_id')->constrained()->onDelete('cascade');
+            $table->string('nid');
             $table->text('qid', 50)->unique();
             $table->text('question_text_en')->nullable();
             $table->text('question_text_in')->nullable();
